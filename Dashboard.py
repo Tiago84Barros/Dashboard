ed2946deade2a853153d0514b5b6c987cd4fbe2c
Tiago84Barros <tsbarros84@yahoo.com.br>
2024-09-17 16:29:31 -0300

    Update Dashboard.py

--- Dashboard.py
+++ Dashboard.py
@@ -5,6 +5,7 @@ import yfinance as yf
 from sklearn.linear_model import LinearRegression
 import numpy as np
 from dotenv import load_dotenv
+import openai
 import os
 
 # Carregar a chave da API do OpenAI
