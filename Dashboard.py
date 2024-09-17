e904b64d455e62787a3d236ae16dc6710357a0cd
Tiago84Barros <tsbarros84@yahoo.com.br>
2024-09-17 00:36:22 -0300

    Update Dashboard.py

--- Dashboard.py
+++ Dashboard.py
@@ -6,7 +6,7 @@ import numpy as np
 
 # Função para obter a URL do logotipo a partir do repositório no GitHub ___________________________________________________________________________________________________________________________________________
 def get_logo_url(ticker):
-    ticker_clean = ticker.replace('.SA', '')  # Remover o sufixo ".SA"
+    ticker_clean = ticker.replace('.SA', '').upper()  # Remover o sufixo ".SA" e garantir que o ticker esteja em maiúsculas
     logo_url = f"https://raw.githubusercontent.com/thefintz/icones-b3/main/icones/{ticker_clean}.png"
     
     # Verificar se o logotipo existe na URL
