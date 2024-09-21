531bbac0141f362e25454d85cf6ec119863d84d5
Tiago84Barros <tsbarros84@yahoo.com.br>
2024-09-21 20:11:39 -0300

    Update Dashboard.py

--- Dashboard.py
+++ Dashboard.py
@@ -90,7 +90,7 @@ with st.sidebar:
 # URL do banco de dados no GitHub
 db_url = "https://raw.githubusercontent.com/Tiago84Barros/Dashboard/main/indicadores_empresas.db"
 
-GITHUB_TOKEN = os.getenv('GITHUB_TOKEN')
+GITHUB_TOKEN = "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"
 
 @st.cache_data
 def download_db_from_github(db_url, local_path='indicadores_empresas.db'):
