57cad48aa940cf98189a4a546722c220f7f5049f
Tiago <tsbarros84@yahoo.com.br>
2024-10-17 18:35:56 -0300

    Update Dashboard.py

--- Dashboard.py
+++ Dashboard.py
@@ -445,13 +445,13 @@ st.markdown("### Visão Geral (CAGR)")
 col1, col2, col3 = st.columns(3)
 
 with col1:
-    st.markdown(f"<div class='cagr-box'>CAGR Receita Líquida: {cagrs['Receita_Liquida']:.2%}</div>", unsafe_allow_html=True)
+    st.markdown(f"<div class='cagr-box'>Receita Líquida: {cagrs['Receita_Liquida']:.2%}</div>", unsafe_allow_html=True)
 
 with col2:
-    st.markdown(f"<div class='cagr-box'>CAGR Lucro Líquido: {cagrs['Lucro_Liquido']:.2%}</div>", unsafe_allow_html=True)
+    st.markdown(f"<div class='cagr-box'>Lucro Líquido: {cagrs['Lucro_Liquido']:.2%}</div>", unsafe_allow_html=True)
 
 with col3:
-    st.markdown(f"<div class='cagr-box'>CAGR Dividendos: {cagrs['Dividendos']:.2%}</div>", unsafe_allow_html=True)
+    st.markdown(f"<div class='cagr-box'>Dividendos: {cagrs['Dividendos']:.2%}</div>", unsafe_allow_html=True)
 
 
 # Seletor para escolher quais variáveis visualizar no gráfico _______________________________________________________________________________________________________________________________________
