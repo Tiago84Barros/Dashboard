cb71ad2a8f225f29d35eedc211d29e1683d33d52
Tiago <tsbarros84@yahoo.com.br>
2024-10-21 11:53:43 -0300

    Update Dashboard.py

--- Dashboard.py
+++ Dashboard.py
@@ -617,7 +617,7 @@ if multiplos is not None and not multiplos.empty:
         """, unsafe_allow_html=True)
 
     # Coluna 2 - Margem Operacional
-    with col1:
+    with col2:
         margem_Operacional = multiplos['Margem_Operacional'].fillna(0).values[0]
         st.markdown(f"""
         <div class='metric-box'>
@@ -649,7 +649,7 @@ if multiplos is not None and not multiplos.empty:
      # Segunda linha de colunas
     col5, col6, col7, col8 = st.columns(4)
 
-    # Coluna 4 - Endividamento Total
+    # Coluna 5 - Endividamento Total
     with col5:
         endividamento_total = multiplos['Endividamento_Total'].fillna(0).values[0]
         st.markdown(f"""
@@ -659,8 +659,8 @@ if multiplos is not None and not multiplos.empty:
         </div>
         """, unsafe_allow_html=True)
 
-     # Coluna 5 - Índice de Endividamento sobre o Patrimônio Líquido
-    with col5:
+     # Coluna 6 - Índice de Endividamento sobre o Patrimônio Líquido
+    with col6:
         Indice_endividamento = multiplos['Indice_Endividamento'].fillna(0).values[0]
         st.markdown(f"""
         <div class='metric-box'>
@@ -669,10 +669,32 @@ if multiplos is not None and not multiplos.empty:
         </div>
         """, unsafe_allow_html=True)
 
-   
+     # Coluna 7 - Dívida Líquida
+    with col7:
+        Divida_Liquida = multiplos['Divida_Liquida'].fillna(0).values[0]
+        st.markdown(f"""
+        <div class='metric-box'>
+            <div class='metric-value'>{Divida_Liquida:.2f}</div>
+            <div class='metric-label'>Dívida Líquida</div>
+        </div>
+        """, unsafe_allow_html=True)
 
-    # Coluna 6 - Dividend Yield
-    with col5:
+   
+    # Coluna 8: Líquidez Corrente
+    with col7:
+        Liquidez_Corrente = multiplos['Liquidez_Corrente'].fillna(0).values[0]
+        st.markdown(f"""
+        <div class='metric-box'>
+            <div class='metric-value'>{Liquidez_Corrente:.2f}</div>
+            <div class='metric-label'>Dívida Líquida</div>
+        </div>
+        """, unsafe_allow_html=True)
+
+    # Terceira linha de colunas
+    col9, col10, col11, col12 = st.columns(4)
+
+    # Coluna 9 - Dividend Yield
+    with col9:
         dividend_yield = (multiplos['Dividendo_Yield'].fillna(0).values[0])/current_price
         st.markdown(f"""
         <div class='metric-box'>
@@ -681,8 +703,8 @@ if multiplos is not None and not multiplos.empty:
         </div>
         """, unsafe_allow_html=True)
 
-    # Coluna 6 - P/VP
-    with col6:
+    # Coluna 10 - P/VP
+    with col10:
         pvp = current_price/multiplos['P/VP'].fillna(0).values[0]
         st.markdown(f"""
         <div class='metric-box'>
@@ -691,8 +713,8 @@ if multiplos is not None and not multiplos.empty:
         </div>
         """, unsafe_allow_html=True)
 
-    # Coluna 7 - Payout
-    with col7:
+    # Coluna 11 - Payout
+    with col11:
         payout = (multiplos['Payout'].fillna(0).values[0])*100
         st.markdown(f"""
         <div class='metric-box'>
@@ -701,8 +723,8 @@ if multiplos is not None and not multiplos.empty:
         </div>
         """, unsafe_allow_html=True)
 
-    # Coluna 8 - P/L
-    with col8:
+    # Coluna 12 - P/L
+    with col12:
         pl = current_price/multiplos['P/L'].fillna(0).values[0]
         st.markdown(f"""
         <div class='metric-box'>
