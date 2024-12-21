670d76f89611a542b8f38fb91666e2305794418d
Tiago <tsbarros84@yahoo.com.br>
2024-12-21 10:03:51 -0300

    Update Dashboard.py

--- Dashboard.py
+++ Dashboard.py
@@ -1078,7 +1078,7 @@ if pagina == "Avançada": #_____________________________________________________
                     
                         # Normalizar métricas em relação ao setor (se disponível)
                         setor_empresas = empresas_filtradas[empresas_filtradas['SETOR'] == row['SETOR']]
-                        setor_dfp = dfp[dfp['ticker'].isin(setor_empresas['ticker'])]
+                        setor_dfp = dfp[dfp['Ticker'].isin(setor_empresas['ticker'])]
                         setor_multiplos = multiplos[multiplos['ticker'].isin(setor_empresas['ticker'])]
                     
                         # Métricas normalizadas (usar média do setor como referência)
