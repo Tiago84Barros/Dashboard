bf7f83677b8e32cf3e5bc9a4c7d759e8fd264173
Tiago <tsbarros84@yahoo.com.br>
2025-01-23 09:54:01 -0300

    Update Dashboard.py

--- Dashboard.py
+++ Dashboard.py
@@ -824,7 +824,7 @@ if pagina == "Básica":
                 <div class='metric-box'>
                     <div class='metric-value'>{endividamento_total:.2f}</div>
                      <div class='metric-label' title='Mede o nível de dívida da empresa em relação ao seus ativos                         
-                         ([(Ativo circulante + Ativo Não Circulante) - Passivo Não Financeiro]/Ativo Total).'>Endividamento Total</div>
+                    ([(Ativo circulante + Ativo Não Circulante) - Passivo Não Financeiro]/Ativo Total).'>Endividamento Total</div>
                 </div>
                 """, unsafe_allow_html=True)
         
@@ -835,7 +835,7 @@ if pagina == "Básica":
                 <div class='metric-box'>
                     <div class='metric-value'>{alavancagem_financeira:.2f}</div>
                     <div class='metric-label' title='Mede o quanto a empresa utiliza de capital de terceiros em relação ao patrimônio líquido                            
-                        ([(Ativo circulante + Ativo Circulante] - Passivo Não Financeiro]/Patrimônio Líquido).'>Alavancagem Financeira</div>
+                    ([(Ativo circulante + Ativo Não Circulante] - Passivo Não Financeiro]/Patrimônio Líquido).'>Alavancagem Financeira</div>
                 </div>
                 """, unsafe_allow_html=True)
         
