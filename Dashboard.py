6a96ac21ac667b87605a40a42a7d3afd7dd6d80c
Tiago <tsbarros84@yahoo.com.br>
2025-02-17 18:39:43 -0300

    Update Dashboard.py

--- Dashboard.py
+++ Dashboard.py
@@ -1698,7 +1698,7 @@ if pagina == "Avançada": #_____________________________________________________
                 
                     # 1. BAIXANDO IBOVESPA ____________________________________________________________________________________________________________________________________
                     try:
-                        ibov = yf.download("^BVSP", start="2020-01-01", end="2024-01-01")["Close"]
+                        ibov = yf.download("^BVSP", start="2019-01-01", end="2025-01-01")["Close"]
                     except Exception as e:
                         st.error(f"❌ Erro ao baixar IBOVESPA: {e}")
                         continue
