a2ada04de48e46777842c1efdaeaa84d807257de
Tiago <tsbarros84@yahoo.com.br>
2025-02-17 19:01:44 -0300

    Update Dashboard.py

--- Dashboard.py
+++ Dashboard.py
@@ -1698,14 +1698,15 @@ if pagina == "Avançada": #_____________________________________________________
                 
                     # 1. BAIXANDO IBOVESPA ____________________________________________________________________________________________________________________________________
                     try:
-                        ibov = yf.download("^BVSP", start="2020-01-01", end="2025-01-01")["Close"]
+                        ibov = yf.download("^BVSP", start="2020-01-01", end="2015-01-01")["Close"]
                     except Exception as e:
                         st.error(f"❌ Erro ao baixar IBOVESPA: {e}")
                         continue
                 
                     # 2. BAIXANDO OS PREÇOS DAS EMPRESAS FILTRADAS ______________________________________________________________________________________________________________
                     try:
-                        precos = yf.download(tickers, start="2020-01-01", end="2025-01-01")["Close"]
+                        precos = yf.download(tickers, start="2015-01-01", end="2025-01-01")["Close"]
+                        precos = precos.reindex(pd.date_range(start="2015-01-01", end="2024-01-01", freq="B"))  # Preencher com NaN
                     except Exception as e:
                         st.error(f"❌ Erro ao baixar os preços das empresas: {e}")
                         continue
