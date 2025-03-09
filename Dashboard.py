c057be4f78f70849154140370f1e58cb257cb00e
Tiago <tsbarros84@yahoo.com.br>
2025-03-09 04:50:59 -0300

    Update Dashboard.py

--- Dashboard.py
+++ Dashboard.py
@@ -1205,11 +1205,11 @@ if pagina == "Avançada": #_____________________________________________________
         multiplos['Ano'] = pd.to_datetime(multiplos['Data'], errors='coerce').dt.year
         df_dre['Ano'] = pd.to_datetime(df_dre['Data'], errors='coerce').dt.year
         anos_disponiveis = sorted(multiplos['Ano'].unique())
-        st.dataframe(anos_disponiveis)
-    
+          
         for ano in anos_disponiveis[:-1]:  # não calcula no último ano disponível (não tem como prever ano seguinte)
             df_multiplos_acum = multiplos[multiplos['Ano'] <= ano]
             df_dre_acumulado = dre[dre['Ano'] <= ano]
+            st.dataframe(df_dre_acumulado)
     
             metricas = calcular_metricas_historicas_simplificadas(df_mult=df_multiplos[df_multiplos['Ano'] <= ano],
                                                                    df_dre=df_dre[df_dre['Ano'] <= ano])
