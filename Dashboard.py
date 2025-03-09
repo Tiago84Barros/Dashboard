b5460fc537b8bd0be4b726c9f847e96ab211fdf1
Tiago <tsbarros84@yahoo.com.br>
2025-03-09 12:38:04 -0300

    Update Dashboard.py

--- Dashboard.py
+++ Dashboard.py
@@ -1444,10 +1444,10 @@ if pagina == "Avançada": #_____________________________________________________
                  
                     # DataFrame com scores
                     df_scores = pd.concat([pd.DataFrame(res) for res in resultados])
-                    """                                       
+                                                 
                     # Determinar líderes
                     lideres_por_ano = determinar_lideres(df_scores)
-                    
+                    """
                     # Baixar preços
                     precos = baixar_precos([ticker + ".SA" for ticker in empresas_filtradas['ticker']])
                     
