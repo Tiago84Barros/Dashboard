bc66c4b48293863e27c954eb58346fbc857b47b7
Tiago <tsbarros84@yahoo.com.br>
2025-03-25 18:16:36 -0300

    Update Dashboard.py

--- Dashboard.py
+++ Dashboard.py
@@ -1905,7 +1905,9 @@ if pagina == "Avançada": #_____________________________________________________
                     
                     # Baixar preços
                     precos = baixar_precos([ticker + ".SA" for ticker in empresas_filtradas['ticker']])
-                    st.dataframe(lista_empresas)                    
+                    
+                    st.dataframe(lista_empresas)   
+                    
                     # Escores das empresas de acordo com segmento e tipo de empresa
                     df_scores = calcular_score_acumulado(lista_empresas, dados_macro, anos_minimos=4)
                                                                                   
