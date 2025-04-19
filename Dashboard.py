85b6709a29b971153b0b9b1147af77e7240a8fff
Tiago <tsbarros84@yahoo.com.br>
2025-04-19 14:01:09 -0300

    Update Dashboard.py

--- Dashboard.py
+++ Dashboard.py
@@ -10,7 +10,7 @@ from collections import defaultdict
 import numpy as np
 import sqlite3
 import openai
-import o
+import os
 
 # Função para obter a URL do logotipo a partir do repositório no GitHub ___________________________________________________________________________________________________________________________________________
 
