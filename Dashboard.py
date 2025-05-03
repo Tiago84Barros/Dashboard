2c5357a77aeefc070e220557b8b09dc80c9f3fae
Tiago <tsbarros84@yahoo.com.br>
2025-05-03 18:43:56 -0300

    Update Dashboard.py

--- Dashboard.py
+++ Dashboard.py
@@ -9,7 +9,7 @@ Script principal que instancia a UI e delega às páginas modularizadas
 from __future__ import annotations
 
 import streamlit as st
-import pandas as pd
+import pandas aspd
 
 # ---------------------------------------------------------------------------
 # Módulos internos -----------------------------------------------------------
