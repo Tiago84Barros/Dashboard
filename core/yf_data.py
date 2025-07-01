c8979b9410460ed17a50abc7d833888b2b4f2a7a
Tiago <tsbarros84@yahoo.com.br>
2025-06-30 21:52:29 -0300

    Update yf_data.py

--- core/yf_data.py
+++ core/yf_data.py
@@ -84,7 +84,7 @@ def baixar_precos(tickers, start="2010-01-01"):
     """
     st.write("Ticker", tickers)
     try:
-        precos = yf.download(tickers, start=start, end="2025-12-31", interval="1d", group_by='ticker')
+        precos = yf.download(tickers, start=start, end="2025-12-31", auto_adjust=True, progress=False)["Close"]
         precos.columns = precos.columns.str.replace(".SA", "", regex=False)  # Ajustar tickers
         
         # Remover linhas onde todos os preços são NaN (empresas sem dados nesse período)
