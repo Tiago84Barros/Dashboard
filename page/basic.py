c7be18f21246b829269e143b0e8eb8a42c7628e7
Tiago <tsbarros84@yahoo.com.br>
2025-12-21 20:27:16 -0300

    Update basic.py

--- page/basic.py
+++ page/basic.py
@@ -1,8 +1,4 @@
 with st.sidebar:
-    if st.button("Atualizar dados", key="refresh_button"):
-        st.cache_data.clear()
-        st.experimental_rerun()
-
     ticker_input = st.text_input("Buscar ticker (ex.: PETR4)", key="ticker_box")
     if ticker_input.strip():
         ticker = ticker_input.upper()
