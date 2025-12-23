f1abaa25f8a1608a2bd4384586b719e72f423107
Tiago <tsbarros84@yahoo.com.br>
2025-12-23 13:58:33 -0300

    Update empresa_view.py

--- page/empresa_view.py
+++ page/empresa_view.py
@@ -5,7 +5,7 @@ import pandas as pd
 import streamlit as st
 import plotly.express as px
 
-from core.helpers import get_company_info, get_logo_url
+from analytics.helpers import get_company_info, get_logo_url
 from core.db.engine import get_engine
 from core.db.loader import (
     load_demonstracoes_financeiras,
