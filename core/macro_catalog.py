50ed6a4351c53d2d166c076a5ef209875fb76c44
Tiago <tsbarros84@yahoo.com.br>
2025-12-25 17:20:02 -0300

    Create macro_catalog.py

---
+++ core/macro_catalog.py
@@ -0,0 +1,37 @@
+BCB_SERIES_CATALOG = {
+    "IPCA_MENSAL": {
+        "sgs": 433,
+        "freq": "monthly",
+        "descricao": "IPCA mensal (%)"
+    },
+    "SELIC_META": {
+        "sgs": 432,
+        "freq": "daily",
+        "descricao": "Meta da taxa Selic (%)"
+    },
+    "SELIC_EFETIVA": {
+        "sgs": 4189,
+        "freq": "daily",
+        "descricao": "Taxa Selic efetiva (%)"
+    },
+    "CAMBIO_PTX": {
+        "sgs": 10813,
+        "freq": "daily",
+        "descricao": "Câmbio PTAX venda"
+    },
+    "ICC": {
+        "sgs": 4393,
+        "freq": "monthly",
+        "descricao": "Índice de Confiança do Consumidor"
+    },
+    "PIB": {
+        "sgs": 4380,
+        "freq": "quarterly",
+        "descricao": "PIB real (R$ milhões)"
+    },
+    "BALANCA_COMERCIAL": {
+        "sgs": 22707,
+        "freq": "monthly",
+        "descricao": "Balança comercial (US$ milhões)"
+    },
+}
