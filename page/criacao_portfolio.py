326e89faaa8cd293140b9edd680820695b84dc72
Tiago <tsbarros84@yahoo.com.br>
2026-01-21 11:44:36 -0300

    Update criacao_portfolio.py

--- page/criacao_portfolio.py
+++ page/criacao_portfolio.py
@@ -52,7 +52,7 @@ from page.portfolio_patches import (
     render_patch2_dominancia,
     render_patch3_stress_test,
     render_patch4_diversificacao,
-    render_patch5_benchmark_segmento,
+
 )
 
 # <<<
@@ -660,6 +660,3 @@ def render():
     render_patch2_dominancia(score_global, lideres_global, empresas_lideres_finais)
     render_patch3_stress_test(score_global, lideres_global, empresas_lideres_finais)
     render_patch4_diversificacao(empresas_lideres_finais, contrib_globais)
-    render_patch5_benchmark_segmento(score_global, empresas_lideres_finais)
-
-
